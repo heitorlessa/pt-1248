6c040fab2c92fbd9973c68d23090ab87d330f965
heitorlessa <lessa@amazon.co.uk>
2022-06-16 11:50:37 +0200

    fix: set body to empty string also works

--- src/app.py
+++ src/app.py
@@ -11,12 +11,14 @@ app = ALBResolver(cors=CORSConfig(max_age=1))
 def get_hello_universe():
     return {"message": "hello universe"}
 
-@app.route(rule=".*", method="OPTIONS")
-def preflight():
-    return {"message": "hello CORS"}
+# @app.route(rule=".*", method="OPTIONS")
+# def preflight():
+#     return {"message": "hello CORS"}
 
 
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.APPLICATION_LOAD_BALANCER)
 @tracer.capture_lambda_handler
 def lambda_handler(event, context):
-    return app.resolve(event, context)
+    ret = app.resolve(event, context)
+    ret["body"] = ""
+    return ret
